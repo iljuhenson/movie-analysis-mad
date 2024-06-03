85653fc2b803652ed6fbcde719944b10bcfab396
AdamRopelewski <adam.ropelewski.kontakt@gmail.com>
2024-06-03 21:34:01 +0200

    white spaces fixed

--- machine_learning.py
+++ machine_learning.py
@@ -15,25 +15,43 @@ scaler = MinMaxScaler()
 
 
 # One-hot encoding categorical features
-categorical_features_reg = encoder.fit_transform(movies_df[["adult", "genres", "original_language", "spoken_languages", "production_countries",]])
-scaled_features_reg = scaler.fit_transform(movies_df[["budget", "release_date", "runtime", "vote_count", 'revenue']])
+categorical_features_reg = encoder.fit_transform(
+    movies_df[
+        [
+            "adult",
+            "genres",
+            "original_language",
+            "spoken_languages",
+            "production_countries",
+        ]
+    ]
+)
+scaled_features_reg = scaler.fit_transform(
+    movies_df[["budget", "release_date", "runtime", "vote_count", "revenue"]]
+)
 X = np.hstack([scaled_features_reg, categorical_features_reg])
 y = movies_df["avg_of_rating"].values
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
 
 # Neural Network for Regression
-model_reg = tf.keras.Sequential([
-    tf.keras.layers.Dense(64, activation='relu', input_shape=(X_train.shape[1],)),
-    tf.keras.layers.Dense(32, activation='relu'),
-    tf.keras.layers.Dense(1)
-])
+model_reg = tf.keras.Sequential(
+    [
+        tf.keras.layers.Dense(64, activation="relu", input_shape=(X_train.shape[1],)),
+        tf.keras.layers.Dense(32, activation="relu"),
+        tf.keras.layers.Dense(1),
+    ]
+)
 
-model_reg.compile(optimizer='adam', loss='mean_squared_error')
+model_reg.compile(optimizer="adam", loss="mean_squared_error")
 model_reg.summary()
 
 # Training the Model
-history_reg = model_reg.fit(X_train, y_train, epochs=50, batch_size=32, validation_split=0.2, verbose=1)
+history_reg = model_reg.fit(
+    X_train, y_train, epochs=100, batch_size=32, validation_split=0.2, verbose=1
+)
 
 # Evaluating the Model
 y_pred = model_reg.predict(X_test)
