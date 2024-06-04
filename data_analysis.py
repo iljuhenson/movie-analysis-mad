c915c388a61fb89cfd8be245a28e15a3075cff6b
AdamRopelewski <adam.ropelewski.kontakt@gmail.com>
2024-06-04 22:40:58 +0200

    refactor to use constatnts file

--- data_analysis.py
+++ data_analysis.py
@@ -1,25 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from constants import NUMERICAL_COLUMNS
 
 file_path = "output/movies_relevant_data.csv"
 movies_df = pd.read_csv(file_path)
 
-numerical_columns = [
-    "avg_of_rating",
-    "adult",
-    "budget",
-    "genres",
-    "original_language",
-    "release_date",
-    "revenue",
-    "spoken_languages",
-    "runtime",
-    "production_countries",
-    "vote_count",
-]
+
 plt.figure(figsize=(15, 10))
-for i, col in enumerate(numerical_columns, 1):
+for i, col in enumerate(NUMERICAL_COLUMNS, 1):
     plt.subplot(3, 4, i)
     sns.histplot(movies_df[col])
     plt.title(f"Distribution of {col}")
