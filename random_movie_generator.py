926fe680b6f6a11e37d681d9c5c2813f94abf13b
AdamRopelewski <adam.ropelewski.kontakt@gmail.com>
2024-06-06 22:29:57 +0200

    dont use int. dropedd columns that are not features

--- random_movie_generator.py
+++ random_movie_generator.py
@@ -3,19 +3,23 @@ import numpy as np
 
 
 def generate_random_values(num_samples=10):
-    df = pd.read_csv('./output/movies_relevant_data_num_ids.csv')
+    df = pd.read_csv("./output/movies_relevant_data_num_ids.csv")
+    df.drop("avg_of_rating", axis=1, inplace=True)  # Drop the target column
+    df.drop(
+        "movieId_movies_metadata", axis=1, inplace=True
+    )  # Drop the movieId column (it's not a feature
     random_movies = []
     columns = df.columns
 
     for _ in range(num_samples):
         movie = {}
         for column in columns:
-            if df[column].dtype in [np.int64, np.int32]:
-                movie[column] = np.random.randint(df[column].min(), df[column].max())
-            elif df[column].dtype in [np.float64, np.float32]:
+            if column == "avg_of_rating":
                 movie[column] = np.random.uniform(df[column].min(), df[column].max())
+            else:
+                movie[column] = int(np.random.uniform(min(df[column]), max(df[column])))
         random_movies.append(movie)
-    
+
     return pd.DataFrame(random_movies)
 
 
